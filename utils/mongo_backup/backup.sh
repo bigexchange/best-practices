5879742c078e7600b31843e9de1432edc85a5dcc
Morgan Gnanamani <morgan.gnanamani@gmail.com>
2019-07-15 16:41:32 +0800

    Add a new Script and README

--- utils/mongo_backup/backup.sh
+++ utils/mongo_backup/backup.sh
@@ -1,38 +1,42 @@
+#set -x
 #!/bin/bash
-#edited by morgan 
-#removed hardcoded passwords as a first requirement
-if [ $# != 2 ]; then
-     echo "Your command should contain 2 arguments."
-     echo "Use the following syntax: backup.sh <mongo user> <mongo password>"
-     exit
-fi
 
-BIGID_MONGO_USER=$1
-BIGID_MONGO_PWD=$2
-echo 'for better security it is recommended to chmod 600 the file, and rename it to start with a "."'
-sleep 5
+BIGID_MONGO_USER=someusername
+BIGID_MONGO_PWD=somepassword
+SSL=y
+SSL_TRUE=y
+CA_PEM=ca.pem
+SERVER_PEM=server.pem
 
 DIR=$(date +%m-%d-%Y_%H_%M_%S)
 BACKUP_ROOT=~/db_backups
 DEST=$BACKUP_ROOT/$DIR
 SCRIPT_PATH=$BACKUP_ROOT/bin
-mkdir -p $DEST
-mkdir -p $SCRIPT_PATH
-cp backup.sh $SCRIPT_PATH
+
+if [ "$SSL_TRUE" = "SSL" ]
+then
+
+docker exec bigid-mongo /bin/bash -c 'mongodump -h bigid-mongo:27017 -u '$BIGID_MONGO_USER' -p '$BIGID_MONGO_PWD' --ssl --sslCAFile=$CA_PEM --sslPEMKeyFile=$SERVER_PEM --authenticationDatabase=admin --gzip > backup_local.sh'
+
+else
+
 docker exec bigid-mongo /bin/bash -c 'mongodump -h localhost -u '$BIGID_MONGO_USER' -p '$BIGID_MONGO_PWD' --authenticationDatabase=admin --gzip > backup_local.sh'
+
+fi
+
 docker cp bigid-mongo:/dump $DEST
 cd $BACKUP_ROOT && tar -zcvf $DIR.tar.gz $DIR
-docker exec bigid-mongo /bin/bash -c 'cd /dump && rm -r *'
+docker exec bigid-mongo /bin/bash -c `cd /dump && rm -r *`
 rm -r $DIR
 
-#delete all backup files except the newest 3.
+#delete all backup files except the newest 3
 ls *.tar.gz -t | tail -n +4 | xargs rm --
 
 crontab -l > mycron
 
 #backup every weekend at 6 AM
- # m h dom mon dow command
-grep -qxF "0 06 * * 7 $SCRIPT_PATH/db_backup.sh" mycron || echo  "0 06 * * 7 $SCRIPT_PATH/db_backup.sh $BIGID_MONGO_USER $BIGID_MONGO_PWD" >> mycron
+# m h dom mon dow command
+grep -qxF "0 06 * * 7 $SCRIPT_PATH/backup.sh" mycron || echo  "0 06 * * 7 $SCRIPT_PATH/backup.sh $BIGID_MONGO_USER $BIGID_MONGO_PWD" >> mycron
 crontab mycron
 
 echo -e "\n\nbackup is finished and scheduled with the following crontab:\n"
